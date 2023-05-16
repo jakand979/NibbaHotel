6206df1989a7ca7cab09ab523cc5b2644de020ec
jakand979 <jak.and@interia.pl>
2023-05-16 09:29:53 +0200

    Adding image. Updating CSS.

--- index.php
+++ index.php
@@ -2,22 +2,21 @@
 <html lang="en">
 <head>
     <link rel="icon" type="image/ico" href="favicon/favicon.ico">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&family=Roboto&display=swap" >
     <link rel="stylesheet" type="text/css" href="css/home-styles.css">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nibba Hotel - Most Comfortable</title>
+    <title>Nibba Hotel - We Value Your Comfort</title>
 </head>
 <body>
     <nav class="home-nav-container">
-        <a href="#"><img src="images/logo.png" alt="logo"></a>
+        <a href="#"><h1>NIBBA HOTEL</h1></a>
         <a href="#">About</a>
-        <a href="#">Offers</a>
-        <a href="#">Navigation</a>
+        <a href="#">Rooms</a>
+        <a href="#">Availability</a>
         <a href="#">Contact</a>
         <button><a href="sign-in.php">Sign in</a></button>
     </nav>
+    <div class="main-content">
+
+    </div>
 </body>
 </html>
