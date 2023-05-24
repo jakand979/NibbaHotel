b0a3223f1b2d470b7da578f959543b5b92de001b
jakand979 <jak.and@interia.pl>
2023-05-25 01:43:42 +0200

    Little changes in about us description.

--- index.php
+++ index.php
@@ -38,17 +38,18 @@
     </div>
     <div class="about-us">
         <h1>Welcome to Sunny Hotel Nibba!</h1>
-        <span>We invite you to an unforgettable experience at our luxurious oasis of relaxation, where the sun, beautiful beaches, and excellent service create a unique atmosphere of comfort and pleasure.
-            As you step through our doors, you will be greeted by our friendly staff, ready to fulfill your every wish.
-            Our elegant lobby impresses with its spaciousness and décor inspired by the local culture, creating an atmosphere of tranquility and comfort.
-            Sunny Hotel Nibba offers a wide selection of luxury rooms and suites, designed with meticulous attention to detail.
-            Each room is spacious, air-conditioned, and equipped with state-of-the-art amenities such as a flat-screen TV, minibar, and complimentary Wi-Fi.
-            Moreover, most of our rooms feature private balconies with sea views, where you can relax and enjoy the breathtaking scenery.
-            Our pool complex is a true haven for water enthusiasts. Immerse yourself in our stunning Olympic-sized pool, unwind in the jacuzzi, or enjoy refreshing drinks at our poolside bar.
-            For those who prefer direct contact with the sea, we invite you to our private beach, where white sand and turquoise water provide unforgettable moments of relaxation and pleasure.
-            Our restaurant is a true paradise for your taste buds.
-            Our talented team of chefs serves international and local cuisine, prepared with the freshest ingredients.
-            In our elegant interior or on the restaurant terrace, you can indulge in exquisite dishes and fine wines that enhance the flavor of your exceptional stay.</span>
+        <span>We were here since <b><u>1969</u></b>, to serve our visitors the best! We have <b><u>multiple hotels</u></b> in various <b><u>coastal regions</u></b> around the <b><u>world</u></b>!
+            We want to invite you to an <b><u>unforgettable experience</u></b> at our luxurious oasis of relaxation, where the sun, beautiful beaches, and <b><u>excellent service</u></b> create a <b><u>unique atmosphere</u></b> of comfort and pleasure.
+            As you step through our doors, you will be greeted by our <b><u>friendly staff</u></b>, ready to fulfill your every wish.
+            Our <b><u>elegant lobby</u></b> impresses with its spaciousness and décor inspired by the local culture, creating an atmosphere of tranquility and comfort.
+            Sunny <b><u>Hotel Nibba</u></b> offers a <b><u>wide selection</u></b> of luxury rooms and suites, designed with <b><u>meticulous attention</u></b> to detail.
+            Each room is spacious, air-conditioned, and equipped with <b><u>state-of-the-art amenities</u></b> such as a flat-screen TV, minibar, and complimentary Wi-Fi.
+            Moreover, most of our rooms feature <b><u>private balconies</u></b> with <b><u>sea views</u></b>, where you can relax and enjoy the <b><u>breathtaking scenery</u></b>.
+            Our <b><u>pool complex</u></b> is a <b><u>true haven</u></b> for water enthusiasts. Immerse yourself in our stunning <b><u>Olympic-sized pool</u></b>, unwind in the <b><u>jacuzzi</u></b>, or enjoy <b><u>refreshing drinks</u></b> at our poolside bar.
+            For those who prefer direct contact with the sea, we invite you to our private beach, where white sand and turquoise water provide <b><u>unforgettable moments</u></b> of relaxation and pleasure.
+            Our restaurant is a <b><u>true paradise</u></b> for your taste buds.
+            Our <b><u>talented team</u></b> of chefs serves international and local cuisine, prepared with the <b><u>freshest ingredients</u></b>.
+            In our <b><u>elegant interior</u></b> or on the restaurant terrace, you can indulge in <b><u>exquisite dishes</u></b> and <b><u>fine wines</u></b> that enhance the flavor of your <b><u>exceptional stay</u></b>.</span>
     </div>
     <footer>
 
