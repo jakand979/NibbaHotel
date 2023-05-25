7e8a33297daa317aa1195d716ef6a296a5fb1263
jakand979 <jak.and@interia.pl>
2023-05-26 00:09:46 +0200

    Renaming css file, adding many images to index.php, finishing index.php design

--- index.php
+++ index.php
@@ -41,18 +41,34 @@
         <span>We were here since <b><u>1969</u></b>, to serve our visitors the best! We have <b><u>multiple hotels</u></b> in various <b><u>coastal regions</u></b> around the <b><u>world</u></b>!
             We want to invite you to an <b><u>unforgettable experience</u></b> at our luxurious oasis of relaxation, where the sun, beautiful beaches, and <b><u>excellent service</u></b> create a <b><u>unique atmosphere</u></b> of comfort and pleasure.
             As you step through our doors, you will be greeted by our <b><u>friendly staff</u></b>, ready to fulfill your every wish.
-            Our <b><u>elegant lobby</u></b> impresses with its spaciousness and décor inspired by the local culture, creating an atmosphere of tranquility and comfort.
+            Our <b><u>elegant lobbies</u></b> impress with their spaciousness and décor inspired by the local culture, creating an atmosphere of tranquility and comfort.
             Sunny <b><u>Hotel Nibba</u></b> offers a <b><u>wide selection</u></b> of luxury rooms and suites, designed with <b><u>meticulous attention</u></b> to detail.
             Each room is spacious, air-conditioned, and equipped with <b><u>state-of-the-art amenities</u></b> such as a flat-screen TV, minibar, and complimentary Wi-Fi.
-            Moreover, most of our rooms feature <b><u>private balconies</u></b> with <b><u>sea views</u></b>, where you can relax and enjoy the <b><u>breathtaking scenery</u></b>.
-            Our <b><u>pool complex</u></b> is a <b><u>true haven</u></b> for water enthusiasts. Immerse yourself in our stunning <b><u>Olympic-sized pool</u></b>, unwind in the <b><u>jacuzzi</u></b>, or enjoy <b><u>refreshing drinks</u></b> at our poolside bar.
-            For those who prefer direct contact with the sea, we invite you to our private beach, where white sand and turquoise water provide <b><u>unforgettable moments</u></b> of relaxation and pleasure.
-            Our restaurant is a <b><u>true paradise</u></b> for your taste buds.
-            Our <b><u>talented team</u></b> of chefs serves international and local cuisine, prepared with the <b><u>freshest ingredients</u></b>.
-            In our <b><u>elegant interior</u></b> or on the restaurant terrace, you can indulge in <b><u>exquisite dishes</u></b> and <b><u>fine wines</u></b> that enhance the flavor of your <b><u>exceptional stay</u></b>.</span>
+            Moreover, most of our rooms feature <b><u>private balcony</u></b> with <b><u>sea views</u></b>, where you can relax and enjoy the <b><u>breathtaking scenery</u></b>.
+            Our <b><u>pool complexes</u></b> are a <b><u>true haven</u></b> for water enthusiasts. Immerse yourself in our stunning <b><u>Olympic-sized pools</u></b>, unwind in the <b><u>jacuzzis</u></b>, or enjoy <b><u>refreshing drinks</u></b> at our poolside bars.
+            For those who prefer direct contact with the sea, we invite you to our private beaches, where white sand and turquoise water provide <b><u>unforgettable moments</u></b> of relaxation and pleasure.
+            Our restaurants are a <b><u>true paradise</u></b> for your taste buds.
+            Our <b><u>talented teams</u></b> of chefs serves international and local cuisine, prepared with the <b><u>freshest ingredients</u></b>.
+            In our <b><u>elegant interiors</u></b> or on the restaurant terraces, you can indulge in <b><u>exquisite dishes</u></b> and <b><u>fine wines</u></b> that enhance the flavor of your <b><u>exceptional stay</u></b>.</span>
     </div>
-    <footer>
-
+    <div class="facilites">
+        <img src="images/hotel.png" class="item" alt="wifi_sign">
+        <img src="images/deck-chair.png" class="item" alt="sunny_deckchair_beach">
+        <img src="images/receptionist.png" class="item" alt="reception_sign"
+        <img src="" class="" alt="">
+        <img src="images/bellboi.png" class="item" alt="bell_boy">
+        <img src="images/maid.png" class="item" alt="maid">
+        <img src="images/television.png" class="item" alt="TV">
+        <img src="images/minibar.png" class="item" alt="minibar">
+        <img src="images/wifi.png" class="item" alt="wifi_sign">
+    </div>
+    <footer class="footer-style">
+        <img src="images/pin.png" class="footer-item" alt="location_pin">
+        <p class="footer-item">2801 Hill St, New Smyrna Beach, FL 32169, United States</p>
+        <img src="images/phone-call.png" class="footer-item" alt="phone_call">
+        <p class="footer-item">(555) 555-1234</p>
+        <img src="images/email.png" class="footer-item" alt="phone_call">
+        <p class="footer-item">hotelnibba.global@hotels.com</p>
     </footer>
 </body>
 </html>
--- sign-up.php
+++ sign-up.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <link rel="icon" type="image/ico" href="favicon/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="css/sign-forms.css">
+    <link rel="stylesheet" type="text/css" href="css/forms.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nibba Hotel</title>
